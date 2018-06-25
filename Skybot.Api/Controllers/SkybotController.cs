7da35de63baef20328f155a218fc196f36fa81f6
Malek Atwiz <matwiz@futurecode.ca>
2018-06-24 21:07:08 -0400

    integrated Auth0 for authorization

--- Skybot.Api/Controllers/SkybotController.cs
+++ Skybot.Api/Controllers/SkybotController.cs
@@ -1,5 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using Skybot.Api.Services;
+using Skybot.Models.Skybot;
 
 namespace Skybot.Api.Controllers
 {
@@ -8,17 +16,44 @@ namespace Skybot.Api.Controllers
     public class SkybotController : ControllerBase
     {
         private readonly IRecognitionService recognitionService;
+        private readonly IConfiguration configuration;
 
-        public SkybotController(IRecognitionService recognitionService)
+        public SkybotController(IConfiguration configuration, IRecognitionService recognitionService)
         {
+            this.configuration = configuration;
             this.recognitionService = recognitionService;
         }
 
         [Route("process")]
         [HttpPost]
-        public IActionResult Process([FromBody]dynamic message)
+        [Authorize]
+        public IActionResult Process([FromBody]RecognitionQuery message)
         {
-            return Ok(recognitionService.Process(message.query.ToString()));
+            return Ok(recognitionService.Process(message.Message));
+        }
+
+        [AllowAnonymous]
+        [Route("token")]
+        [HttpPost]
+        public async Task<IActionResult> RequestToken(string clientId, string clientSecret, string identifier)
+        {
+            var httpClient = new HttpClient();
+
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var requestContent = new
+            {
+                grant_type = "client_credentials",
+                client_id = clientId,
+                client_secret = clientSecret,
+                audience = identifier
+            };
+
+            var response = await httpClient.PostAsJsonAsync($"https://{configuration["Auth0:Domain"]}/oauth/token", requestContent);
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            var tokenObject = JsonConvert.DeserializeObject<dynamic>(responseContent);
+
+            return tokenObject == null ? StatusCode((int)HttpStatusCode.Unauthorized) : (IActionResult)Ok(tokenObject.access_token);
         }
     }
 }
--- Skybot.Api/Startup.cs
+++ Skybot.Api/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -21,12 +22,24 @@ namespace Skybot.Api
         {
             services.AddTransient<IRecognitionService, RecognitionService>();
 
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = $"https://{Configuration["Auth0:Domain"]}/";
+                options.Audience = Configuration["Auth0:ApiIdentifier"];
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseAuthentication();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
