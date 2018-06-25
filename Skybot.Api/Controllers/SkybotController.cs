8a3d5bc8ee9d6489038e34645c1b45c979fb0868
Malek Atwiz <matwiz@futurecode.ca>
2018-06-24 20:12:39 -0400

    Skybot API

---
+++ Skybot.Api/Controllers/SkybotController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Skybot.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class SkybotController : ControllerBase
+    {
+    }
+}
