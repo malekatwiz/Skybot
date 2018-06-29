8c89e1704ba84c9caed26990b0728964b5c0b7a2
Malek Atwiz <matwiz@futurecode.ca>
2018-06-28 20:40:20 -0400

    Translate intent

--- Skybot.Api/Services/IntentsServices/TranslateIntent.cs
+++ Skybot.Api/Services/IntentsServices/TranslateIntent.cs
@@ -1,14 +1,35 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Google.Cloud.Translation.V2;
+using Microsoft.Extensions.Configuration;
 using Skybot.Models.Skybot;
 
 namespace Skybot.Api.Services.IntentsServices
 {
     public class TranslateIntent : IIntentService
     {
-        public Task<RecognitionResult> Execute(LuisResultModel model)
+        private readonly IConfiguration configuration;
+
+        public TranslateIntent(IConfiguration configuration)
         {
-            throw new NotImplementedException();
+            this.configuration = configuration;
+        }
+
+        public async Task<RecognitionResult> Execute(LuisResultModel model)
+        {
+            var client = TranslationClient.CreateFromApiKey(configuration["TranslateApiCredentials:ApiKey"]);
+            var targetText = model.Entities.FirstOrDefault(x => x.Type.Equals("Dictionary.Text"))?.Name;
+            var targetLanguage = model.Entities.FirstOrDefault(x => x.Type.Equals("Dictionary.TargetLanguage"))?.Name;
+
+            var languages = client.ListLanguages("en");
+            var targetLanguageCode = languages.FirstOrDefault(x => string.Equals(x.Name, targetLanguage, StringComparison.InvariantCultureIgnoreCase));
+            var translation = await client.TranslateTextAsync(targetText, targetLanguageCode?.Code);
+
+            return new RecognitionResult
+            {
+                Message = translation.TranslatedText
+            };
         }
     }
 }
