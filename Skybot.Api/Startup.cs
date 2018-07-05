9dd61ffdeecdf3b7c4f7b401518aa3ea8c62fb27
Malek Atwiz <malekatwiz@gmail.com>
2018-07-05 16:34:28 -0400

    configure logger factory

--- Skybot.Api/Startup.cs
+++ Skybot.Api/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Skybot.Api.Services;
 using Skybot.Api.Services.IntentsServices;
 using Skybot.Api.Services.Settings;
@@ -41,9 +42,12 @@ namespace Skybot.Api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             app.UseAuthentication();
+
+            loggerFactory.AddConsole();
+            loggerFactory.AddDebug();
 
             if (env.IsDevelopment())
             {
