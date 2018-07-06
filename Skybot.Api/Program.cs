9e1d82370baa189357bde4f4ecaa7360db705db6
Malek Atwiz <malekatwiz@gmail.com>
2018-07-06 12:26:07 -0400

    Revert "application insight"
    This reverts commit 38a4001d856bc780230dd486bb45050be0ec0858.

--- Skybot.Api/Program.cs
+++ Skybot.Api/Program.cs
@@ -12,7 +12,6 @@ namespace Skybot.Api
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
