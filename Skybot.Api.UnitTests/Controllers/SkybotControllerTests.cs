6594b42375540c134b65f9a0c774ebd8267e0d1d
Malek Atwiz <malekatwiz@gmail.com>
2019-01-24 22:51:54 -0500

    continue SkybotController unit tests.

--- Skybot.Api.UnitTests/Controllers/SkybotControllerTests.cs
+++ Skybot.Api.UnitTests/Controllers/SkybotControllerTests.cs
@@ -13,6 +13,35 @@ namespace Skybot.Api.UnitTests.Controllers
     public class SkybotControllerTests
     {
         [TestMethod]
+        public async Task Process_ReturnsOkObjectResult_WhenQueryIsNotEmpty()
+        {
+            var query = "I'm testing you";
+            var recognitionResult = new RecognitionResult
+            {
+                Message = "I will pass your test"
+            };
+
+            var recognitionServiceMock = new Mock<IRecognitionService>();
+            recognitionServiceMock.Setup(x => x.Process(query))
+                .Returns(Task.FromResult(recognitionResult))
+                .Verifiable();
+
+            var loggerMock = new Mock<ILogger<SkybotController>>();
+
+            var skybotController = new SkybotController(recognitionServiceMock.Object,
+                loggerMock.Object);
+
+            var result = await skybotController.Process(query);
+            var okObjectResult = result as OkObjectResult;
+
+            recognitionServiceMock.Verify();
+
+            Assert.IsNotNull(okObjectResult);
+            Assert.AreEqual(okObjectResult.StatusCode, 200);
+            Assert.AreEqual(okObjectResult.Value, recognitionResult.Message);
+        }
+
+        [TestMethod]
         public async Task Process_ReturnsHttpBadRequest_WhenQueryIsEmpty()
         {
             var recognitionServiceMock = new Mock<IRecognitionService>();
