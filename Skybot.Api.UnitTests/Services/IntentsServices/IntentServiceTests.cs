20d755dc3302df95ad610a9c8da7b612ece08d4d
Malek Atwiz <malekatwiz@gmail.com>
2019-01-25 01:03:46 -0500

    IntentFactory, IntentService tests.

---
+++ Skybot.Api.UnitTests/Services/IntentsServices/IntentServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Skybot.Api.Models;
+using Skybot.Api.Services.IntentsServices;
+using Skybot.Api.Services.Settings;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Skybot.Api.UnitTests.Services.IntentsServices
+{
+    [TestClass]
+    public class IntentServiceTests
+    {
+        [TestMethod]
+        public async Task Execute_ReturnsNonEmptyRecognitionResult_WhenGivenValidIntent()
+        {
+            var testEntities = new List<LuisEntity>
+            {
+                new LuisEntity{Name = "", Type = "", Score = 0}
+            };
+
+            var settingsMock = new Mock<ISettings>();
+            settingsMock.Setup(x => x.TranslateApiKey).Returns("key");
+
+            var intentFactoryMock = new Mock<IIntentFactory>();
+            intentFactoryMock.Setup(x => x.CreateIntent("Translate"))
+                .Returns(new TranslateIntent(settingsMock.Object));
+
+            var intentService = new IntentService(intentFactoryMock.Object);
+
+            var result = await intentService.Execute("Translate", testEntities);
+
+            Assert.IsNotNull(result);
+            Assert.AreNotEqual(result.Message, string.Empty);
+        }
+    }
+}
--- Skybot.Api/Services/IntentsServices/NoneIntent.cs
+++ Skybot.Api/Services/IntentsServices/NoneIntent.cs
@@ -6,6 +6,11 @@ namespace Skybot.Api.Services.IntentsServices
 {
     public class NoneIntent : Intent
     {
+        public NoneIntent()
+        {
+
+        }
+
         public override Task<RecognitionResult> Execute(IList<LuisEntity> entities)
         {
             return Task.Run(() => new RecognitionResult {Message = "Sorry, I don't understand what you asked me"});
