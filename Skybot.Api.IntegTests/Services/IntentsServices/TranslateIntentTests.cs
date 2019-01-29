998700eb0c6de92b69f25127246c14d5217b9308
Malek Atwiz <malekatwiz@gmail.com>
2019-01-28 21:04:59 -0500

    TranslateIntent integration test.

---
+++ Skybot.Api.IntegTests/Services/IntentsServices/TranslateIntentTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Skybot.Api.IntegTests.Controllers;
+using Skybot.Api.Models;
+using Skybot.Api.Services.IntentsServices;
+using Skybot.Api.Services.Settings;
+
+namespace Skybot.Api.IntegTests.Services.IntentsServices
+{
+    [TestClass]
+    public class TranslateIntentTests : IntegTestBase
+    {
+        [TestMethod]
+        public async Task Execute_ReturnsTranslation_WhenEntitiesHaveTextAndTargetLanguage()
+        {
+            var testEntities = new List<LuisEntity>
+            {
+                new LuisEntity {Score = 1, Type = "Dictionary.Text", Name = "Hello"},
+                new LuisEntity {Score = 1, Type = "Dictionary.TargetLanguage", Name = "French"}
+            };
+
+            var settingsMock = new Mock<ISettings>();
+            settingsMock.Setup(x => x.TranslateApiKey)
+                .Returns(Config["GoogleTarnslateApiKey"]);
+
+            var translateIntent = new TranslateIntent(settingsMock.Object);
+
+            var result = await translateIntent.Execute(testEntities);
+
+            Assert.IsNotNull(result);
+            Assert.IsFalse(string.IsNullOrEmpty(result.Message));
+            Assert.AreEqual(result.Message, "Bonjour");
+        }
+    }
+}
---
+++ Skybot.Api.IntegTests/Services/Luis/LuisServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Skybot.Api.IntegTests.Controllers;
+using Skybot.Api.Services.Luis;
+using Skybot.Api.Services.Settings;
+
+namespace Skybot.Api.IntegTests.Services.Luis
+{
+    [TestClass]
+    public class LuisServiceTests : IntegTestBase
+    {
+        [TestMethod]
+        public async Task Query_ReturnsModelWithIntents_WhenCommandQuered()
+        {
+            var settingsMock = new Mock<ISettings>();
+            settingsMock.Setup(x => x.LuisAppUri)
+                .Returns($"{Config["LuisApp:Uri"]}?subscription-key={Config["LuisApp:Key"]}&verbose=true&timezoneOffset=0");
+
+            var luisService = new LuisService(settingsMock.Object);
+
+            var result = await luisService.Query("Who let the dog out??");
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Intents);
+            Assert.AreNotEqual(result.Intents.Count, 0);
+        }
+    }
+}
